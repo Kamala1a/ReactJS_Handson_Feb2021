5c9e15502943dde27d2bffd6df9fc2b2c0483dab
kamala1a <kamalaabe2010@gmail.com>
2021-02-18 20:35:29 +0530

    react-js

---
+++ src/data.js
@@ -0,0 +1,19 @@
+export default [
+    
+    {
+        id: 1,
+        name: 'Bertie Yates',
+        age: 29,
+        image: 
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg'
+    },
+
+    {
+        id: 1,
+        name: 'Hester Hogan',
+        age: 32,
+        image: 
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg'
+    },
+
+]
